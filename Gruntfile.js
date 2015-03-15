002b4d0a57925b069a684fb93d4401abafcb827f
Steve Roberts <steve@mbudm.com>
2015-03-15 16:03:29 +1100

    Copy index.html and rename app.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,25 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 		browserify: {
-      'build/Release/bundle.js': ['app/app.js']
+      'build/Release/bundle.js': ['src/app.js']
     },
 		watch: {
-      files: [ "app/**/*.js"],
+      files: [ "src/**/*.js"],
       tasks: [ 'browserify' ]
-    }
+    },
+		copy: {
+		  main: {
+				src: 'src/index.html',
+		    dest: 'build/Release/index.html',
+		    options: {
+		      process: function (content, srcpath) {
+		        return content.replace(/\bapp.js\b/g,"bundle.js");
+		      }
+		    }
+			}
+		}
   })
- grunt.loadNpmTasks('grunt-browserify')
- grunt.loadNpmTasks('grunt-contrib-watch')
+ 	grunt.loadNpmTasks('grunt-browserify')
+ 	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-copy')
 }
